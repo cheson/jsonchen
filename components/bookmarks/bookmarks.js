12cc3cc4fe909939c8a20f05f1b061adebbd32b3
Jason Chen <cheson@stanford.edu>
2016-08-18 01:01:48 -0700

    select random link functionality in bookmarks

--- components/bookmarks/bookmarks.js
+++ components/bookmarks/bookmarks.js
@@ -5,22 +5,31 @@ jsonchen.controller('BookmarksController', ['$scope',
             "http://stanford.edu/class/archive/cs/cs106b/cs106b.1142/indent.shtml",
             "http://allrecipes.com/recipe/banana-banana-bread/detail.aspx"
         ];
-    function getDomain(url) {
-        let match = url.match(/:\/\/(.[^/]+)/)[1];
-        console.log(match);
-        return match;
-    }
+        function getDomain(url) {
+            let match = url.match(/:\/\/(.[^/]+)/)[1];
+            console.log(match);
+            return match;
+        };
 
-    $scope.test = function test() {
-    $("a[href^='http']").each(function() {
-        console.log("here!");
-        $(this).css({
-            background: "url(http://favicon.yandex.net/favicon/" + getDomain(this.href) +
-            ") left center no-repeat",
-            "padding-left": "20px"
-        });
-    });
-    }
+        //warn before use that another tab will be opened
+        $scope.selectRandomLink = function() {
+            console.log($scope.bookmarks.length);
+            let index = Math.floor(Math.random() * $scope.bookmarks.length);
+            console.log("index: ", index);
+            console.log("link: ", $scope.bookmarks[index]);
+            window.open($scope.bookmarks[index]);
+        };
+
+        $scope.test = function() {
+            $("a[href^='http']").each(function() {
+                console.log("here!");
+                $(this).css({
+                    background: "url(http://favicon.yandex.net/favicon/" + getDomain(this.href) +
+                    ") left center no-repeat",
+                    "padding-left": "20px"
+                });
+            });
+        }
 
     }]);
 
